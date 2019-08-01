f4c3fb56991539f5652d154bc7f0a588d9ad0576
hardikphp <hardik.mca08@gmail.com>
2019-08-01 12:21:58 +0530

    Chat app initilization

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+
+require("dotenv").config();
+
+var app = express();
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+mongoose.connect(
+    process.env.MONGO_DB_CONNECTION,
+    {
+        useNewUrlParser: true
+    },
+    function(err, db) {
+        if (err) {
+            console.log(
+                "please restart the server and/or check the connection"
+            );
+        } else {
+            console.log("mongodb connected");
+        }
+    }
+);
+
+app.listen(process.env.PORT, () => {
+    console.log("server is running at ", process.env.PORT);
+});
