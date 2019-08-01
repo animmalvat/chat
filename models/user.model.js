dd7da8b655363034783a901682333ca83d61869e
helly patel <hellydotinfotech@gmail.com>
2019-08-01 16:07:43 +0530

    user api

---
+++ models/user.model.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+const user =  new mongoose.Schema ({
+    name: String,
+    email: String,
+    password: String
+});
+
+module.exports = mongoose.model('user' , user)
